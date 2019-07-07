bcad3dccbd0f0c2289165eca610be4486376f953
范承祥 <13640391529@163.com>
2019-07-07 17:34:00 +0800

    F-First Edition

---
+++ MessageBoard.js
@@ -0,0 +1,9 @@
+var id_name=document.getElementById("id");
+
+var id_message=document.getElementById("message");
+
+var id_name_show=document.getElementById("name_show");
+
+var id_message_show=document.getElementById("message_show");
+
+id_name_show=set
