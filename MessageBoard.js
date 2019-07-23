ec0ae86e1a6ec21a83d0272903bedaf595829bb8
范承祥 <13640391529@163.com>
2019-07-23 10:45:28 +0800

    F-MessageBoard Version1.0

--- MessageBoard.js
+++ MessageBoard.js
@@ -1,9 +1,15 @@
-var id_name=document.getElementById("id");
-
-var id_message=document.getElementById("message");
-
-var id_name_show=document.getElementById("name_show");
-
-var id_message_show=document.getElementById("message_show");
-
-id_name_show=set
+function showMessage(){
+	var messageBox=document.getElementById("messageBox").value;
+	var nameBox=document.getElementById("nameBox").value;
+	var message=document.getElementById("message");
+	var name=document.getElementById("name");
+	var time=document.getElementById("time");
+	var date=new Date();
+	var curTime=date.toLocaleString();
+	var messageString="<h3>"+messageBox+"</h3>";
+	message.innerHTML=messageString;
+	var nameString="<h3>"+nameBox+"</h3>";
+	name.innerHTML=nameString;
+	var timeString="<h3>"+curTime+"</h3>";
+	time.innerHTML=timeString;
+}
