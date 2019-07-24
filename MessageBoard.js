e3f67e517d238d9c656339b03140fcfcadf0f942
范承祥 <13640391529@163.com>
2019-07-24 16:15:52 +0800

    F-Version1.3

--- MessageBoard.js
+++ MessageBoard.js
@@ -1,15 +1,12 @@
 function showMessage(){
-	var messageBox=document.getElementById("messageBox").value;
-	var nameBox=document.getElementById("nameBox").value;
+	var messageBox=document.getElementById("messageBox");
+	var nameBox=document.getElementById("nameBox");
 	var message=document.getElementById("message");
 	var name=document.getElementById("name");
 	var time=document.getElementById("time");
 	var date=new Date();
 	var curTime=date.toLocaleString();
-	var messageString="<h3><em>"+messageBox+"</em></h3>";
-	message.innerHTML=messageString;
-	var nameString="<h3><em>"+nameBox+"</em></h3>";
-	name.innerHTML=nameString;
-	var timeString="<h3><em>"+curTime+"</em></h3>";
-	time.innerHTML=timeString;
+	message.firstChild.nodeValue=messageBox.value;
+	name.firstChild.nodeValue=nameBox.value;
+	time.firstChild.nodeValue=curTime;
 }
